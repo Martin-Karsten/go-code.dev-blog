2632e689ed55512a86ada5cc058d3860ddb9241f
Martin <mrtn.karsten@gmail.com>
2023-01-22 16:52:13 +0100

    add blog post: How to compose interfaces in go

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'go-code.dev',
+  author: 'Martin Karsten',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
